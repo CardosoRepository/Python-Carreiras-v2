d4f01a05a0394fab8ec7723c7d5dc76d32307054
Cardoso <gabrielcard0s0@hotmail.com>
2025-05-11 23:50:57 -0300

    fix (main): create table vagas if not exists

--- database.py
+++ database.py
@@ -8,6 +8,18 @@ load_dotenv()
 string_conexao = os.getenv("db_conexao_string")
 engine = create_engine(string_conexao)
 
+with engine.connect() as conn:
+    conn.execute(text("""
+        CREATE TABLE IF NOT EXISTS vagas (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            titulo VARCHAR(250) NOT NULL,
+            localidade VARCHAR(250) NOT NULL,
+            salario INT,
+            moeda VARCHAR(10),
+            responsabilidades VARCHAR(2000)
+        )
+    """))
+	
 def carrega_vagas_db():
 	with engine.connect() as conn:
 		resultado = conn.execute(text("select * from vagas"))
